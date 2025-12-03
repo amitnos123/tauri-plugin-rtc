be51f8f722ac68d10f62ccfbd432ae3f8eb9c80a
sami.nosatzki <sami.nosatzki@gmail.com>
2025-12-03 22:12:19 +0200

    init files

--- guest-js/index.ts
+++ guest-js/index.ts
@@ -3,3 +3,9 @@ import * as TauriRTCPeerConnection from './rtc_peer_connection'
 import * as TauriRTCSctpTransport from './rtc_sctp_transport'
 import * as TauriRTCSessionDescription from './rtc_session_description'
 import * as TauriRTCCertificate from "./rtc_certificate";
+import * as TauriRTCDtlsTransport from "./rtc_dtls_transport";
+import * as TauriRTCIceTransport from "./rtc_ice_transport";
+
+// Method
+// -------
+// getUserMedia()
