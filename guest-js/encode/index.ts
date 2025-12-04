866f787954d41a2645750665be2fe36cf0b44635
sami.nosatzki <sami.nosatzki@gmail.com>
2025-12-04 11:49:20 +0200

    exported all files

--- guest-js/encode/index.ts
+++ guest-js/encode/index.ts
@@ -1,2 +1,4 @@
 export * from './rtc_encoded_audio_frame';
 export * from './rtc_encoded_video_frame';
+export * from './rtc_audio_source_stats';
+export * from './rtc_video_source_stats';
--- guest-js/index.ts
+++ guest-js/index.ts
@@ -1,10 +1,19 @@
-import * as TauriRTCIdentityAssertion from './rtc_identity_assertion'
-import * as TauriRTCPeerConnection from './peer/rtc_peer_connection'
-import * as TauriRTCSctpTransport from './rtc_sctp_transport'
-import * as TauriRTCSessionDescription from './rtc_session_description'
-import * as TauriRTCCertificate from "./rtc_certificate";
-import * as TauriRTCDtlsTransport from "./rtc_dtls_transport";
-import * as TauriRTCIceTransport from "./ice/rtc_ice_transport";
+export * as DataChannel from './data_channel';
+export * as DTMF from './dtmf';
+export * as Encode from './encode';
+export * as Ice from './ice';
+export * as Peer from './peer';
+export * as Rtp from './rtp';
+export * as Certificate from  './rtc_certificate';
+export * as Codec from './rtc_codec_stats';
+export * as Dtls from './rtc_dtls_transport';
+export * as Error from './rtc_error_event';
+export * as IdentityAssertion from './rtc_identity_assertion';
+export * as SCTP from './rtc_sctp_transport';
+export * as Session from'./rtc_session_description';
+export * as StatsReport from './rtc_stats_report';
+export * as TrackEvent from './rtc_track_event';
+export * as Transform from './rtc_transform_event';
 
 // Method
 // -------
