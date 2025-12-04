0ef30ee7db4be461481c9026dced6c87e4fb54f1
sami.nosatzki <sami.nosatzki@gmail.com>
2025-12-04 17:03:33 +0200

    created peer connection ice properties

--- guest-js/peer/rtc_peer_connection_ice_error_event.ts
+++ guest-js/peer/rtc_peer_connection_ice_error_event.ts
@@ -1,3 +1,26 @@
-export default class TauriRTCConnectionIceErrorEvent {
+export default class TauriRTCPeerConnectionIceErrorEvent {
+    private _address: string | null = ""; 
+    private _errorCode: number = 0;
+    private _errorText: string = "";
+    private _port: number = 0;
+    private _url: string = "";
+    get address(): string | null {
+        return this._address;
+    }
+    get errorCode(): number {
+        return this._errorCode;
+    }
+    get errorText(): string | null {
+        return this._errorText;
+    }
+    get port(): number {
+        return this._port;
+    }
+    get url(): string {
+        return this._url;
+    }
 
+    constructor() {
+
+    }
 }
--- guest-js/peer/rtc_peer_connection_ice_event.ts
+++ guest-js/peer/rtc_peer_connection_ice_event.ts
@@ -1,3 +1,10 @@
-export default class TauriRTCConnectionIceEvent {
+export default class TauriRTCPeerConnectionIceEvent {
+    private _candidate: RTCIceCandidate | null = null; // RTCCandidate
+    get candidate(): RTCIceCandidate | null {
+        return this._candidate;
+    }
 
+    constructor() {
+
+    }
 }
