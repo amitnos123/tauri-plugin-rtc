97ce1a158623015e56a21042ccb92432324d8837
sami.nosatzki <sami.nosatzki@gmail.com>
2025-12-10 00:43:35 +0200

    implemented TauriRTCEncodedAudioFrame

--- guest-js/encode/rtc_encoded_audio_frame.ts
+++ guest-js/encode/rtc_encoded_audio_frame.ts
@@ -1,10 +1,29 @@
+export interface TauriRTCEncodedAudioFrameMetadata {
+    audioLevel?: number;
+    captureTime: number;
+    contributingSources?: number[];
+    mimeType: string;
+    payloadType: number;
+    receiveTime?: number;
+    rtpTimestamp: number;
+    sequenceNumber: number;
+    synchronizationSource: number;
+}
+
 export default class TauriRTCEncodedAudioFrame {
     public data : ArrayBuffer;
+    private _metadata: TauriRTCEncodedAudioFrameMetadata;
     
-    constructor() {
 
+    constructor(data: ArrayBuffer, metadata: TauriRTCEncodedAudioFrameMetadata) {
+        this.data = data;
+        this._metadata = metadata;
     }
 
-    // Method
-    // getMetadata()
+    /**
+     * Returns metadata about this encoded audio frame
+     */
+    public getMetadata(): TauriRTCEncodedAudioFrameMetadata {
+        return { ...this._metadata }; // return a copy to prevent mutation
+    }
 }
