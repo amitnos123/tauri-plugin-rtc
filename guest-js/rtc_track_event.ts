d7d5fd81d52894f5df9f58afa6493bc5b900d81d
sami.nosatzki <sami.nosatzki@gmail.com>
2025-12-09 17:16:06 +0200

    implemented TauriRTCTrackEvent

--- guest-js/rtc_track_event.ts
+++ guest-js/rtc_track_event.ts
@@ -1,2 +1,26 @@
-export default class TauriRTCTrackEvent {
+import TauriRTCRTPReceiver from "./rtp/rtc_rtp_receiver";
+import TauriRTCRTPTransceiver from "./rtp/rtc_rtp_transceiver";
+
+export default class TauriRTCTrackEvent extends Event {
+    private _receiver!: TauriRTCRTPReceiver;
+    private _stream?: MediaStream[];
+    private _track!: MediaStreamTrack;
+    private _transceiver!: TauriRTCRTPTransceiver;
+
+    get receiver() : TauriRTCRTPReceiver {
+        return this._receiver;
+    }
+
+    get stream() : MediaStream[] | undefined {
+        return this._stream;
+    }
+
+    get track() : MediaStreamTrack {
+        return this._track;
+    }
+
+    get transceiver() : TauriRTCRTPTransceiver {
+        return this._transceiver;
+    }
+    
 }
