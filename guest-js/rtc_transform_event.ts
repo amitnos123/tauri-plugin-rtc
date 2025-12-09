81f3ed87352bd01de17a75c10c4269171c8eb3f5
sami.nosatzki <sami.nosatzki@gmail.com>
2025-12-09 17:12:00 +0200

    implemented TauriRTCTransformEvent

--- guest-js/rtc_transform_event.ts
+++ guest-js/rtc_transform_event.ts
@@ -1,3 +1,10 @@
-export default class TauriRTCTransformEvent {
+import TauriRTCRTPScriptTransformer from "./rtp/rtc_rtp_script_transformer";
 
+export default class TauriRTCTransformEvent {
+    private _transformer!: TauriRTCRTPScriptTransformer;
+
+    get transformer() : TauriRTCRTPScriptTransformer {
+        return this._transformer;
+    }
+    
 }
