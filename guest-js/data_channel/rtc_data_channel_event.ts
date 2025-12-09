d4fe3b0fba09af3d7a84625acfa5897662276537
sami.nosatzki <sami.nosatzki@gmail.com>
2025-12-09 17:57:52 +0200

    Fixed TauriRTCDataChannelEvent

--- guest-js/data_channel/rtc_data_channel_event.ts
+++ guest-js/data_channel/rtc_data_channel_event.ts
@@ -1,9 +1,38 @@
 import TauriRTCDataChannel from "./rtc_data_channel";
 
-export default class TauriRTCDataChannelEvent {
-    private _channel!: TauriRTCDataChannel;
+/**
+ * Initialization object for a TauriRTCDataChannelEvent.
+ */
+export interface TauriRTCDataChannelEventInit extends EventInit {
+  /** The TauriRTCDataChannel associated with this event */
+  channel: TauriRTCDataChannel;
+}
 
-    get channel(): TauriRTCDataChannel {
-        return this._channel;
-    }
+/**
+ * Event representing a WebRTC-style data channel event in Tauri.
+ * Wraps a TauriRTCDataChannel instance for dispatching via EventTarget.
+ */
+export default class TauriRTCDataChannelEvent extends Event {
+  /** The TauriRTCDataChannel associated with this event */
+  private readonly _channel: TauriRTCDataChannel;
+
+  /**
+   * Creates a new TauriRTCDataChannelEvent.
+   * @param type The event type, e.g., "open", "message", "close".
+   * @param eventInitDict Initialization dictionary containing the channel and optional EventInit fields.
+   */
+  constructor(type: string, eventInitDict: TauriRTCDataChannelEventInit) {
+    // Pass bubbles and cancelable from EventInit if provided
+    super(type, eventInitDict);
+
+    // Assign the channel
+    this._channel = eventInitDict.channel;
+  }
+
+  /**
+   * Returns the TauriRTCDataChannel associated with this event.
+   */
+  get channel(): TauriRTCDataChannel {
+    return this._channel;
+  }
 }
