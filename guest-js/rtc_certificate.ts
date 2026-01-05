184c61cb38e2716394ba0f35bf71956188d8c8cd
sami.nosatzki <sami.nosatzki@gmail.com>
2026-01-05 20:01:46 +0200

    added basic implementation of TauriRTCCertificate

--- guest-js/rtc_certificate.ts
+++ guest-js/rtc_certificate.ts
@@ -1,3 +1,17 @@
-export default class TauriRTCCertificate {
+export interface TauriRTCDtlsFingerprint {
+    algorithm?: string;
+    value?: string;
+}
 
+export default class TauriRTCCertificate {
+    private _expires!: number
+    
+    public get expires() : number {
+        return this._expires;
+    }
+
+    public getFingerprints(): TauriRTCDtlsFingerprint[] {
+        let rtn :TauriRTCDtlsFingerprint[] = [];
+        return rtn;
+    }
 }
