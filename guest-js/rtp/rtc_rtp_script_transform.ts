36cbc24a4fc8782c76b0fa6d9c9c1f37ae02bb65
sami.nosatzki <sami.nosatzki@gmail.com>
2026-02-03 17:40:18 +0200

    implemented TauriRTCRTPScriptTransform

--- guest-js/rtp/rtc_rtp_script_transform.ts
+++ guest-js/rtp/rtc_rtp_script_transform.ts
@@ -1,5 +1,36 @@
 export default class TauriRTCRTPScriptTransform {
-    constructor() {
-        
+    private _worker: Worker; // Thread for transformer
+    private _port: MessagePort;
+
+    get port(): MessagePort {
+        return this._port;
+    }
+
+    constructor(worker: Worker, options: Object = {}, transfer: Transferable[] = []) {
+        if (!worker || typeof (worker as any).postMessage !== "function") {
+            throw new TypeError("Invalid worker passed to RTCRtpScriptTransform");
+        }
+
+        const channel = new MessageChannel();
+
+        this._worker = worker;
+        this._port = channel.port1;
+
+        worker.postMessage(
+            {
+                type: "rtp-script-transform-init",
+                options: options,
+                port: channel.port2
+            },
+            [channel.port2, ...transfer]
+        );
+    }
+
+    // Call when not in use to free up resources
+    close() {
+        if (this._port) {
+            this._port.close();
+            this._port = null!;
+        }
     }
 }
